c4a385435ee82fdb69c9427c79d49c58513dd20e
ben4ali <alibenkarrouch@gmail.com>
2025-01-26 15:43:01 -0500

    Animations dialog

--- vite-project/src/projectDialog.js
+++ vite-project/src/projectDialog.js
@@ -1,9 +1,13 @@
+import gsap from 'gsap';
+
 export default class ProjectDialog {
     constructor() {
         this.dialog = document.querySelector('.project-dialog');
         this.closeButton = this.dialog.querySelector('.close-button');
         this.projectLinks = document.querySelectorAll('.project-link');
         this.projectPreviews = document.querySelectorAll('.project-overview');
+        this.projectCarousel = document.querySelector('.project-carousel');
+        this.projectInfo = document.querySelector('.project-info');
 
         this.projectLinks.forEach(link => {
             link.addEventListener('click', (event) => {
@@ -31,10 +35,16 @@ export default class ProjectDialog {
     }
 
         openDialog() {
-        this.dialog.style.display = 'flex';
+            this.dialog.style.display = 'flex';
+            gsap.set(this.projectCarousel, { y: 600, opacity: 0 });
+            gsap.set(this.projectInfo, { y: -600, opacity: 0 });
+
+            gsap.to(this.projectCarousel, { y: 0, duration: 0.55,opacity: 1, ease: 'sine' });
+            gsap.to(this.projectInfo, { y: 0, duration: 0.55,opacity:1, ease: 'sine' });
         }
     
         closeDialog() {
-        this.dialog.style.display = 'none';
+            gsap.to(this.projectCarousel, { y: -600, duration: 0.35, opacity: 0, ease: 'power2.inOut' });
+            gsap.to(this.projectInfo, { y: 600, duration: 0.35, opacity: 0, ease: 'power2.inOut', onComplete: () => {this.dialog.style.display = 'none';} });
         }
 }
